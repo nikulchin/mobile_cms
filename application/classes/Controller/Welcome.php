cc32cc0369edcb029100b07887e82cc041ab84ef
nikulchin <nikulchin@gmail.com>
2015-05-15 12:02:50 +0300

    Redirect uri FIX

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -17,7 +17,7 @@ class Controller_Welcome extends Controller_Template {
         $content = View::factory('login')
             ->set('social_login_caption','Вход через социальную сеть')
             ->set('google_href','https://accounts.google.com/o/oauth2/auth')
-            ->set('google_redirect_uri','http://m.sharein.ru/auth&response_type=code')
+            ->set('google_redirect_uri','http://m.sharein.ru/auth/google&response_type=code')
             ->set('google_client_id','62701041915-id05msd2pgu9omtcmn5rphhgb9f46sna.apps.googleusercontent.com')
             ->set ('google_scope','https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile');
 
